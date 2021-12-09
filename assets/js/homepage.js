58d92dec25269a9f14ed682abad1e2e58aad2e03
svnday <40030806+svnday@users.noreply.github.com>
2021-12-09 15:54:24 -0600

    function to fetch user data

---
+++ assets/js/homepage.js
@@ -0,0 +1,15 @@
+var getUserRepos = function() {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+};
+
+console.log("outside");
+
+getUserRepos();
